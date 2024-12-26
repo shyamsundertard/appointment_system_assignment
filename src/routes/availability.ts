80aa5baf55c98e68071f32a63f76cc900fbf8401
Shyamsunder Tard <iamshyamsundertard@gmail.com>
2024-12-26 23:47:32 +0530

    Fetching availability slots by professor and student

--- src/routes/availability.ts
+++ src/routes/availability.ts
@@ -4,6 +4,7 @@ import { AuthenticatedRequest, authenticateJWT, authorizeRole } from "../middlew
 import { timeValidation } from "../validators/auth.ts";
 import { validationResult } from "express-validator";
 import { checkAvailabilityOverlap } from "../middlewares/overlapCheck.ts";
+import { Role } from "@prisma/client";
 
 const availabilityRouter = express.Router();
 
@@ -33,14 +34,76 @@ availabilityRouter.get("/availability/id/:id", async (req: Request, res: Respons
             return;
         }
 
-        res.status(500).json(slot);
+        res.status(200).json(slot);
     } catch (error) {
         res.status(500).json(error);
     }
 })
 
+//timeSlots by professorId for professor
+availabilityRouter.get("/availability/professor/timeSlots", authenticateJWT, authorizeRole(Role.PROFESSOR), async (req: AuthenticatedRequest, res: Response): Promise<void> => {
+    try {
+       const professorId = req.user?.id;
+       const slots = await prisma.availability.findMany({ 
+        where: { professorId },
+        include: {
+            appointments: true
+        }
+       });
+       if (!slots || slots.length == 0) {
+        res.status(404).json({ error: "No availability slot found"});
+        return;
+       }
+
+       res.status(200).json({
+        timeSlots: slots.map(slot => ({
+            id: slot.id,
+            startTime: slot.startTime,
+            endTime: slot.endTime,
+            appointments: slot.appointments
+        }))
+       });
+
+       return;
+
+    } catch (error) {
+        res.status(500).json({ error: "Error fetching slots"});
+    }
+})
+
+//timeSlots by professorId for student
+availabilityRouter.get("/availability/student/timeSlots/:professorId", authenticateJWT, authorizeRole(Role.STUDENT), async (req: AuthenticatedRequest, res: Response): Promise<void> => {
+    try {
+       const professorId = req.params.id;
+       const slots = await prisma.availability.findMany({ 
+        where: { professorId },
+        include: {
+            appointments: true
+        }
+       });
+       if (!slots || slots.length == 0) {
+        res.status(404).json({ error: "No availability slot found"});
+        return;
+       }
+
+       res.status(200).json({
+        timeSlots: slots.map(slot => ({
+            id: slot.id,
+            startTime: slot.startTime,
+            endTime: slot.endTime,
+            appointments: slot.appointments
+        }))
+       });
+
+       return;
+
+    } catch (error) {
+        res.status(500).json({ error: "Error fetching slots"});
+    }
+})
+
 //new slot
-availabilityRouter.post("/availability/new", authenticateJWT, authorizeRole("PROFESSOR"), timeValidation, checkAvailabilityOverlap, async (req:AuthenticatedRequest, res: Response): Promise<void> => {
+availabilityRouter.post("/availability/new", authenticateJWT, authorizeRole(Role.PROFESSOR), timeValidation, checkAvailabilityOverlap, async (req:AuthenticatedRequest, res: Response): Promise<void> => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         res.status(400).json({ errors: errors.array() });
